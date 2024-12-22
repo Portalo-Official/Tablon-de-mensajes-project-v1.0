e5e5e52adbfa47cf8788328cba3521b36ddd2641
Portalo <smiguezc01@iesarroyoharnina.es>
2024-12-23 00:27:44 +0100

    Usuario Defined

--- tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/entity/Usuario.java
+++ tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/entity/Usuario.java
@@ -1,5 +1,21 @@
 package com.portalo.tablonmensajes.model.entity;
 
-public class Usuario {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
 
+@Getter
+@Entity
+@Table(name="TB_USUARIO")
+public class Usuario {
+	@Id
+	private Long id;
+	@Column(unique = true, nullable = false, length = 12)
+	private String uuid;
+	private String userName;
+	private String gmail;
+	private String password;
+	private String rol;
 }
