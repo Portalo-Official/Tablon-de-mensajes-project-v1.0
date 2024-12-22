aef187fb5d1d006a4335380620ce6078649c2ab9
Portalo <smiguezc01@iesarroyoharnina.es>
2024-12-23 00:27:52 +0100

    Mensaje Defined

--- tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/entity/Mensaje.java
+++ tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/entity/Mensaje.java
@@ -1,5 +1,25 @@
 package com.portalo.tablonmensajes.model.entity;
 
-public class Mensaje {
+import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name="TB_MENSAJE")
+public class Mensaje {
+	@Id
+	private Long id;
+	@Column(unique = true, nullable = false, length = 36)
+	private String uuid;
+	private String contenido;
+	private LocalDate fechaPublicacion;
+	
+	//RELACIONES
+	private Long postId;
+	private Long usuarioId;
 }
