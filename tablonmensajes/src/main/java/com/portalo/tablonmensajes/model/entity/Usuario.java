d24f4ace880a9336df087224c03c18a4c7c518cc
Portalo <smiguezc01@iesarroyoharnina.es>
2024-12-23 23:41:22 +0100

    Usuario controller implemented

--- tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/entity/Usuario.java
+++ tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/entity/Usuario.java
@@ -8,14 +8,25 @@ import lombok.Getter;
 
 @Getter
 @Entity
-@Table(name="TB_USUARIO")
+@Table(name="TB_USUARIOS")
 public class Usuario {
 	@Id
 	private Long id;
 	@Column(unique = true, nullable = false, length = 12)
 	private String uuid;
-	private String userName;
+	@Column(unique = true, nullable = false, length = 50)
+	private String username;
 	private String gmail;
 	private String password;
 	private String rol;
+	
+	public Usuario(String uuid, String username, String gmail, String password, String rol) {
+		super();
+		this.uuid = uuid;
+		this.username = username;
+		this.gmail = gmail;
+		this.password = password;
+		this.rol = rol;
+	}
+	
 }
