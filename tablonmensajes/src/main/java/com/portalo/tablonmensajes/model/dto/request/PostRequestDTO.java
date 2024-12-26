1bc5ae77d63b3f0fa57f537636ad22629286ac35
Portalo <smiguezc01@iesarroyoharnina.es>
2024-12-27 00:06:22 +0100

    DTOs created

--- tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/dto/request/PostRequestDTO.java
+++ tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/dto/request/PostRequestDTO.java
@@ -1,5 +1,21 @@
 package com.portalo.tablonmensajes.model.dto.request;
 
-public class PostRequestDTO {
+import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class PostRequestDTO {
+	@JsonProperty("post_reference")
+	private String uuid;
+	@JsonProperty("title")
+	private String titulo;
+	@JsonProperty("body")
+	private String contenido;
+	
 }
--- tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/dto/response/PostResponseDTO.java
+++ tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/dto/response/PostResponseDTO.java
@@ -1,5 +1,27 @@
 package com.portalo.tablonmensajes.model.dto.response;
 
-public class PostResponseDTO {
+import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PostResponseDTO {
+	@JsonProperty("post_reference")
+	private String uuid;
+	@JsonProperty("title")
+	private String titulo;
+	@JsonProperty("body")
+	private String contenido;
+	@JsonProperty("publish_date")
+	private LocalDate fechaPublicacion;
+	@JsonProperty("usuario_resume")
+	private UsuarioResumeDTO usuario;
 }
