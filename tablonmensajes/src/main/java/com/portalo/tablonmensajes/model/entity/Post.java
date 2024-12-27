422a5e41322b81c5c3b1b3d348bc2f3cd59d3a80
Portalo <smiguezc01@iesarroyoharnina.es>
2024-12-27 23:56:09 +0100

    Post add @ManyToONe

--- tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/entity/Post.java
+++ tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/entity/Post.java
@@ -2,9 +2,13 @@ package com.portalo.tablonmensajes.model.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 @Getter
@@ -20,9 +24,12 @@ public class Post {
 	private String contenido;
 	private LocalDate fechaPublicacion;
 	
-	private Long usuarioId;
-	
-	private Usuario usuario;
+    @Column(name = "usuario_id", insertable = false, updatable = false)
+    private Long usuarioId;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
 
 	public Post( String uuid, String titulo, String contenido, LocalDate fechaPublicacion, Long usuarioId) {
 		super();
