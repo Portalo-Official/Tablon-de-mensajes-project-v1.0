3a108387e256b48b239314fc3bd49ece9e1a63b1
Portalo <smiguezc01@iesarroyoharnina.es>
2024-12-28 02:47:31 +0100

    PostController buscarPorReferencia implemented

--- tablonmensajes/src/main/java/com/portalo/tablonmensajes/controller/PostController.java
+++ tablonmensajes/src/main/java/com/portalo/tablonmensajes/controller/PostController.java
@@ -2,21 +2,33 @@ package com.portalo.tablonmensajes.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.portalo.tablonmensajes.model.dto.response.PostDetailDTO;
+import com.portalo.tablonmensajes.service.post.PostQueryService;
+
+import lombok.AllArgsConstructor;
+
 @RestController
-@RequestMapping("Post")
+@AllArgsConstructor
+@RequestMapping("posts")
 public class PostController {
 	
+	private final PostQueryService postService;
 	
 	/**
 	 * <h1>REQ_P_006 Consultar Post por Referencia</h1>
 	 * @return
 	 */
-	@GetMapping("get")
-	private ResponseEntity<Object> getPostPorReferencia() {
+	@GetMapping("{post_referencia}")
+	private ResponseEntity<Object> getPostPorReferencia(
+			@PathVariable("post_referencia") String postReferencia
+			) {
 		
-		return ResponseEntity.ok("Todo Okey");
+		PostDetailDTO post = postService.buscarPostPorReferencia(postReferencia);
+		
+		return ResponseEntity.ok(postReferencia);
 	}
 }
