976b9398fa5b1134ad54dd07594af2b4ec211ed7
Portalo <smiguezc01@iesarroyoharnina.es>
2024-12-28 02:46:44 +0100

    PostQuery add CategoriaMapper

--- tablonmensajes/src/main/java/com/portalo/tablonmensajes/service/post/impl/PostQueryServiceImpl.java
+++ tablonmensajes/src/main/java/com/portalo/tablonmensajes/service/post/impl/PostQueryServiceImpl.java
@@ -1,12 +1,15 @@
 package com.portalo.tablonmensajes.service.post.impl;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.portalo.tablonmensajes.model.dto.response.CategoriaDTO;
 import com.portalo.tablonmensajes.model.dto.response.PostDetailDTO;
 import com.portalo.tablonmensajes.model.dto.response.UsuarioResumeDTO;
 import com.portalo.tablonmensajes.model.entity.Post;
+import com.portalo.tablonmensajes.model.mapper.CategoriaMapper;
 import com.portalo.tablonmensajes.model.mapper.PostMapper;
 import com.portalo.tablonmensajes.model.mapper.UsuarioMapper;
 import com.portalo.tablonmensajes.repository.PostRepositroy;
@@ -21,12 +24,15 @@ public class PostQueryServiceImpl implements PostQueryService {
 	private final PostRepositroy postRepositroy;
 	private final PostMapper postMapper;
 	private final UsuarioMapper usuarioMapper;
+	private final CategoriaMapper categoriaMapper;
 	@Override
 	public PostDetailDTO buscarPostPorReferencia(String referencia) {
 		Optional<Post> postOptional = this.postRepositroy.buscarPostPorReferencia(referencia);
 		if(postOptional.isPresent()) {
+			
+			List<CategoriaDTO> categoriasDto=  categoriaMapper.entityListToDtoList(postOptional.get().getCategorias());
 			UsuarioResumeDTO usuarioResume= usuarioMapper.entityToResume(postOptional.get().getUsuario());
-			return postMapper.postToPostDetailDTO(postOptional.get(), usuarioResume);
+			return postMapper.postToPostDetailDTO(postOptional.get(), usuarioResume, categoriasDto);
 		}
 		return null;
 	}
