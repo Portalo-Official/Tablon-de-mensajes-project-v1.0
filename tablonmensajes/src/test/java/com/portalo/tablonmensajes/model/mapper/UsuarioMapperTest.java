afc01fae0d6385e5d5119a3477fd717d40233774
Portalo <smiguezc01@iesarroyoharnina.es>
2024-12-28 02:46:56 +0100

    UserMApper Test init

---
+++ tablonmensajes/src/test/java/com/portalo/tablonmensajes/model/mapper/UsuarioMapperTest.java
@@ -0,0 +1,19 @@
+package com.portalo.tablonmensajes.model.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class UsuarioMapperTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void testEntityToResume() {
+		fail("Not yet implemented");
+	}
+
+}
