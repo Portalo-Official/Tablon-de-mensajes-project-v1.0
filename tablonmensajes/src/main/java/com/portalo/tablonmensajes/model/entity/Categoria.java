d21034392f83e562664318fd1da982e842439f7d
Portalo <smiguezc01@iesarroyoharnina.es>
2024-12-28 01:44:28 +0100

    Categoria add entity

--- tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/entity/Categoria.java
+++ tablonmensajes/src/main/java/com/portalo/tablonmensajes/model/entity/Categoria.java
@@ -1,5 +1,21 @@
 package com.portalo.tablonmensajes.model.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name="TB_CATEGORIAS")
 public class Categoria {
 
+	@Id
+	private Integer id;
+	@Column(unique = true, nullable = true, length = 50)
+	private String nombre;
+	
 }
